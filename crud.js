74d57c4c2753b6a0462bb044d3bd13a1fbf4c5e7
Lucas Pires Silva <118729077+LucasVPSilva@users.noreply.github.com>
2023-08-15 15:49:31 -0300

    conclusao do crud

--- crud.js
+++ crud.js
@@ -1,126 +1,187 @@
-const tasks = [
-    { title: 'Correr', description: 'Correr na academia', completed: false },
-    { title: 'Nadar', description: 'Nadar na academia', completed: false },
 
-];
-
-
-// Menu:
+const users = [];
 
 let continuar = true;
-let checkIndex;
-do {
-    const menu = parseInt(prompt(`Digite uma opção:
-    1 - Cadastrar tarefa
-    2 - Listar tarefas
-    3 - Atualizar status tarefa
-    4 - Atualizar tarefa 
-    5 - Excluir tarefa
-    6 - Sair do programa`))
 
-    switch (menu) {
+//--------------------------- Menu de login para o usuário:
+
+
+do {
+    const menuUser = parseInt(prompt(`Digite uma opção:
+    1 - Criar usuário
+    2 - Logar na conta
+    3 - Sair do programa`));
+
+    switch (menuUser) {
         case 1:
-            createTask(prompt('Digite o nome da tarefa:'), prompt('Digite a descrição da tarefa:'))
+            createUser(prompt('Informe o nome do usuário'), prompt('Crie uma senha:'));
             break;
 
         case 2:
-            listTasks();
-            break;
+            const user = login(
+                prompt('Informe o nome do usuário'),
+                prompt('Digite sua senha:'));
 
-        case 3:
-            checkIndex = parseInt(prompt('Informe o índice da tarefa:'));
-            if (!tasks[checkIndex]) {
-                alert('Indice não encontrado!')
-            } else {
-                updateTaskStatus(checkIndex,
-                    confirm('Você concluiu a tarefa?')
-                )
-            };
-            break;
-
-        case 4:
-            checkIndex = parseInt(prompt('Informe o índice da tarefa:'));
-            if (!tasks[checkIndex]) {
-                alert('Indice não encontrado!')
-            } else {
-                updateTask(checkIndex, prompt('Informe o novo nome da tarefa:'),
-                    prompt('Informe a nova descrição da tarefa:'));
-
-            };
-            break;
-
-        case 5:
-            checkIndex = parseInt(prompt('Informe o índice da tarefa:'));
-            if (!tasks[checkIndex]) {
-                alert('Indice não encontrado!')
-            } else {
-                deleteTask(checkIndex);
+            if (user) {
+                managerTasks(user)
             }
             break;
-        case 6:
+        case 3:
             continuar = false;
             break;
-        default:
-            break;
     }
-
 
 
 } while (continuar)
 
 
+// --------------------------- Funções do usuário:
+
+function createUser(username, password) {
+    if (!username || !password) {
+        alert('Usuário e a senha devem ser informados!')
+        return
+    }
+    users.push({ username, password, tasks: [] });
+    alert('Usuário criado com sucesso!')
+
+}
+
+function login(username, password) {
+    const user = users.find(user => user.username === username)
+    if (!user) {
+        alert('Usuário não localizado!')
+        return
+    }
+    if (user.password !== password) {
+        alert('Senha incorreta!')
+        return
+    }
+
+    alert(`Seja bem-vindo(a) ${user.username}`)
+    return user
+
+}
+
+
+// --------------------------- Menu após usuario logar:
+
+
+
+
+function managerTasks(user) {
+
+    do {
+        const menu = parseInt(prompt(`Digite uma opção:
+    1 - Cadastrar tarefa
+    2 - Listar tarefas
+    3 - Atualizar status tarefa
+    4 - Atualizar tarefa 
+    5 - Excluir tarefa
+    6 - Sair do programa`));
+
+        switch (menu) {
+            case 1:
+                createTask(prompt('Digite o nome da tarefa:'), prompt('Digite a descrição da tarefa:'), user);
+                break;
+
+            case 2:
+                listTasks(user);
+                break;
+
+            case 3:
+                updateTaskStatus(parseInt(prompt('Informe o índice da tarefa:')),
+                    confirm('Você concluiu a tarefa?'), user);
+                break;
+
+            case 4:
+                updateTask(parseInt(prompt('Informe o índice da tarefa:')),
+                    prompt('Informe o novo nome da tarefa:'),
+                    prompt('Informe a nova descrição da tarefa:'),
+                    user);
+
+                break;
+
+            case 5:
+                deleteTask(parseInt(prompt('Informe o índice da tarefa:')), user);
+
+                break;
+
+            case 6:
+
+                continuar = false;
+                break;
+            default:
+                break;
+        }
+
+
+
+    } while (continuar)
+
+}
+
 
 
 // Funções da Tasks
 
-function createTask(title, description) {
+function createTask(title, description, user) {
     if (!title) {
         alert('Gentileza informar o nome da tarefa!')
         return;
     }
-    tasks.push({ title, description, completed: false })
+    user.tasks.push({ title, description, completed: false })
     alert('Tarefa incluida com sucesso!')
 }
 
-function listTasks() {
-    if (tasks.length <= 0) {
+function listTasks(user) {
+    if (user.tasks.length <= 0) {
         alert('Não existe nenhuma tarefa cadastrada!')
         return;
     }
-    tasks.forEach((task, index) => {
+    user.tasks.forEach((task, index) => {
         alert(`Índice: ${index} | Título: ${task.title} | Descrição: ${task.description} | 
         Status: ${task.completed ? 'Concluída' : 'Não concluída'}`);
     })
 }
 
 
-function updateTaskStatus(index, completed) {
-    if (!tasks[index]) {
+function updateTaskStatus(index, completed, user) {
+    if (!user.tasks[index]) {
         alert('Indice não encontrado!')
         return
     }
     if (completed) {
-        tasks[index].completed = completed;
+        user.tasks[index].completed = completed;
         alert(`Status atualizado com sucesso!`)
+        return
     }
 
 }
 
-function updateTask(index, newTitle, newDescription) {
+function updateTask(index, newTitle, newDescription, user) {
+    if (!user.tasks[index]) {
+        alert('Indice não encontrado!')
+        return
+    }
     if (!newTitle) {
         alert('Novo título da tarefa deve ser informado!')
         return
     }
-    tasks[index].title = newTitle
+    user.tasks[index].title = newTitle
     alert(`Titulo atualizado com sucesso!`)
     if (newDescription) {
-        tasks[index].description = newDescription
+        user.tasks[index].description = newDescription
         alert(`Descrição atualizada com sucesso!`)
         return;
     }
 }
 
-function deleteTask(index) {
-    tasks.splice(index, 1)
+function deleteTask(index, user) {
+    if (!user.tasks[index]) {
+        alert('Indice não encontrado!')
+        return
+    }
+    user.tasks.splice(index, 1)
     alert(`Tarefa deletada com sucesso!`)
 }
